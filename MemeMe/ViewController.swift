20b80761763e0c88e9afc3f1c59f2e233b01e646
Julia Rozum <rozumjuli@gmail.com>
2022-01-26 20:41:26 +0100

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Julia Rozum on 26/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
