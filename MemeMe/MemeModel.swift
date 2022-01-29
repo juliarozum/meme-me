55e23bec87a5bb6461b994ae72701ba95609629f
Julia Rozum <rozumjuli@gmail.com>
2022-01-29 18:24:21 +0100

    Добавила возможность делиться мемом и сохранять его

---
+++ MemeMe/MemeModel.swift
@@ -0,0 +1,22 @@
+//
+//  MemeModel.swift
+//  MemeMe
+//
+//  Created by Julia Rozum on 28/01/2022.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
+
+
+
+
+
+
